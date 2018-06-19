2020f447557514287d1c3f08dc951703e7b09953
iVesperX <vesperx9@outlook.com>
2018-06-19 02:58:56 -0400

    added givedata.js

---
+++ commands/givedata.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const config = require('../config.json');
+
+const JsonDB = require('node-json-db');
+const db = new JsonDB('data', true, true);
+
+const CODE = '```';
+
+exports.run = ((client, message, args) => {
+  fs.readFile('../data.json', (err, data) => {
+    client.fetchUser(config.ownerID).then(user => {
+      user.send(`${CODE}json\n${data + CODE}`);
+    })
+  });
+});
