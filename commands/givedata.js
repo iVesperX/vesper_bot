5a1e8fb6db2122f78eeca22f077d155162c431bc
iVesperX <vesperx9@outlook.com>
2018-06-19 17:19:37 -0400

    changes to data parsing

--- commands/givedata.js
+++ commands/givedata.js
@@ -5,9 +5,18 @@ const JsonDB = require('node-json-db');
 const db = new JsonDB('data', true, true);
 
 const CODE = '```';
+const max_text_length = 2000;
 
 exports.run = ((client, message, args) => {
   if (message.author.id != config.ownerID) return;
+
+  const parse_and_send = (async (user, text) => {
+    for (let i = 0; i < text.length; i += max_text_length) {
+      let new_text = text.slice(i, i + max_text_length);
+
+      await user.send(`${CODE}json\n${new_text + CODE}`);
+    }
+  });
 
   fs.readFile('data.json', (err, data) => {
     client.fetchUser(config.ownerID).then(user => {
