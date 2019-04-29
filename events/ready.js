a26849c70d0e1be49bfb40d97f7fa89e12bb6069
iVesperX <vesperx9@outlook.com>
2019-04-28 21:17:45 -0400

    Added deployment verification

--- events/ready.js
+++ events/ready.js
@@ -8,12 +8,14 @@ const init = require('../util/init.js');
 exports.run = (async (client) => {
   console.log('Vesper locked and loaded.');
 
-  client.fetchUser(config.ownerID).then(owner => {
-    const today = new Date(),
-          formatted_date = today.toLocaleString('en-US', config.date_options) + ', ' + today.toLocaleTimeString();
-    
-    owner.send(`Successfully deployed on ${formatted_date}`);
-  });
+  if (client.deployed) {
+    client.fetchUser(config.ownerID).then(owner => {
+      const today = new Date(),
+            formatted_date = today.toLocaleString('en-US', config.date_options) + ', ' + today.toLocaleTimeString();
+      
+      owner.send(`Successfully deployed on ${formatted_date}`);
+    });
+  }
 
   let initialized = await client.database.collection('init').findOne({});
   (!initialized || !initialized.data || initialized.data !== true) ? init.initialize.all(client) : console.log('Data has already been initialized');
