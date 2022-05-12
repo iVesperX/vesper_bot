e05f76088e5dad6553a57362e596dfab94bc14a4
iVesperX <vesperx9@outlook.com>
2022-05-12 18:23:59 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -73,8 +73,7 @@ export const run = (async (client, message, args) => {
           const formatted_date = today.toLocaleString('en-US', config.date_options) + ', ' + today.toLocaleTimeString();
           const ver_message = `${message.author.tag} (<@${message.author.id}>) has verified as \`${account.login}\` (on ${formatted_date})`;
 
-          const bot_server = await client.guilds.fetch(config.bot_server.id);
-          bot_server.available && bot_server.channels.fetch(config.bot_server.mod.pl_verification).send(ver_message);
+          client.guilds.fetch(config.bot_server.id).channels.fetch(config.bot_server.mod.pl_verification).send(ver_message);
         }
 
         message.reply(`you\'ve been successfully verified as \`${account.login}\`!`);
